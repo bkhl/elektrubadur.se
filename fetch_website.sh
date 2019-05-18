f1cf4ef0f6688a07b25695c434775bc02b59163d
Björn Lindström <bkhl@elektrubadur.se>
2019-05-18 12:16:12 +0700

    Added script to fetch and deploy website build
    Closed #38

---
+++ fetch_website.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# Usage: fetch_website.sh URL DIRECTORY
+#
+# URL - URL, pointing to ZIP file containing single directory named 'public'.
+# DIRECTORY - Target directory, to deploy the web site to.
+
+main() {
+    local artefact_url=$1
+    local destination=$2
+
+    local temp_zip_file
+    temp_zip_file=$(mktemp --suffix=.zip)
+
+    local temp_dir
+    temp_dir=$(mktemp -d)
+    
+    local public_dir="$temp_dir/public"
+
+    curl --silent --location "$artefact_url" > "$temp_zip_file"
+
+    unzip -q -d "$temp_dir" "$temp_zip_file"
+
+    rsync --archive --quiet --delete-delay "$public_dir/" "$destination"
+}
+
+main "$@"
