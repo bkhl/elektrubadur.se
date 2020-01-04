73d06ca88005ead2e39458ba9752a24f51ed371b
Björn Lindström <bkhl@elektrubadur.se>
2020-01-04 10:57:36 +0700

    Simplified build script
    * Removed home-made serve-locally function, as the built-in one in Zola
      seems to work well enough now.
    * Removed the function to replace __PREFIX__ with the actual URL, as
      Zola now does that properly everywhere.

--- build.py
+++ build.py
@@ -1,44 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-
 from plumbum import cli, local
 from plumbum.cmd import tidy, xmlstarlet, zola
 
-PORT = 8000
-
 
 class Builder(cli.Application):
-    serve_locally = cli.Flag(
-        ["l", "serve locally"], help="Build and serve page locally"
-    )
-
-    TCPServer.allow_reuse_address = True
-
-    def serve(self):
-        with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
-            with local.cwd(local.cwd / "public"):
-                httpd.serve_forever()
-
     def public_files_by_extension(self, *extensions):
         for filepath in (local.cwd / "public").walk(
             filter=lambda f: f.is_file() and f.suffix in ("." + e for e in extensions)
         ):
             yield filepath
-
-    def replace_prefix(self):
-        if self.serve_locally:
-            prefix = f"http://localhost:{PORT}"
-        else:
-            prefix = "https://elektrubadur.se"
-
-        for filepath in self.public_files_by_extension("html", "xml", "txt", "css"):
-            with filepath.open() as file_object:
-                s = file_object.read().replace("__PREFIX__", prefix)
-            with filepath.open("w") as file_object:
-                file_object.write(s)
 
     def clean_html(self):
         files = list(self.public_files_by_extension("html"))
@@ -60,12 +32,8 @@ class Builder(cli.Application):
 
     def main(self):
         zola("build")
-        self.replace_prefix()
         self.clean_html()
         self.clean_xml()
-
-        if self.serve_locally:
-            self.serve()
 
 
 if __name__ == "__main__":
